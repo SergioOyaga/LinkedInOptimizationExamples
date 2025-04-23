92673d89dd42927461fa9d637b7110b1188031a6
Sergio Oyaga <44228600+SergioOyaga@users.noreply.github.com>
2025-04-24 00:06:41 +0200

    Tango MathModel Main run

---
+++ src/main/java/org/soyaga/examples/Tango/MathModelTango/RunLinkedInTango.java
@@ -0,0 +1,10 @@
+package org.soyaga.examples.Tango.MathModelTango;
+
+
+public class RunLinkedInTango {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.startApp();
+    }
+}
