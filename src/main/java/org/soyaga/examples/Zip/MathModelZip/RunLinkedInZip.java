65202d379440a73cbf438286e9dbecd222e2a746
Sergio Oyaga <44228600+SergioOyaga@users.noreply.github.com>
2025-04-25 11:55:42 +0200

    Zip Run Main

---
+++ src/main/java/org/soyaga/examples/Zip/MathModelZip/RunLinkedInZip.java
@@ -0,0 +1,10 @@
+package org.soyaga.examples.Zip.MathModelZip;
+
+
+public class RunLinkedInZip {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.startApp();
+    }
+}
