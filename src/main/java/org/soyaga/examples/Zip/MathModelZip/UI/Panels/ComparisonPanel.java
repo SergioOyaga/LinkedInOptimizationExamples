b3eb63704da569eefc31bf809d8a37de01caa070
Sergio Oyaga <44228600+SergioOyaga@users.noreply.github.com>
2025-04-25 11:54:06 +0200

    Zip UserInterface UI

---
+++ src/main/java/org/soyaga/examples/Zip/MathModelZip/UI/Panels/ComparisonPanel.java
@@ -0,0 +1,85 @@
+package org.soyaga.examples.Zip.MathModelZip.UI.Panels;
+
+import org.soyaga.examples.Zip.MathModelZip.UI.Buttons.SquareButton;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * Panel composed of two panels, reference and optimization.
+ */
+public class ComparisonPanel extends JSplitPane {
+    /**
+     * ReferencePanel to show the LinkedInImages.
+     */
+    private final ReferencePanel referencePanel;
+    /**
+     * OptimizationPanel to show the optimization problem/results
+     */
+    private final OptimizationPanel optimizationPanel;
+
+    /**
+     * Constructor
+     */
+    public ComparisonPanel() {
+        super();
+        this.referencePanel = new ReferencePanel();
+        this.optimizationPanel = new OptimizationPanel();
+    }
+
+    /**
+     * Function that initializes the JPanel components.
+     */
+    public void initComponent() {
+        this.referencePanel.initComponent();
+        this.optimizationPanel.initComponent();
+
+        this.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+        this.setLeftComponent(this.referencePanel);
+        this.setRightComponent(this.optimizationPanel);
+    }
+
+    /**
+     * Function that forwards the sets of the grid buttons
+     * @param colSize int with the col size of the grid
+     * @param rowSize int with the row size of the grid
+     */
+    public void setGridButtons(int rowSize, int colSize){
+        this.optimizationPanel.setGridButtons(rowSize, colSize);
+        this.optimizationPanel.revalidate();
+        this.optimizationPanel.repaint();
+    }
+
+    /**
+     * Function that forwards the set of a solution to the current grid.
+     * @param solution Boolean[][] with the solution.
+     */
+    public void setSolution(Integer[][] solution){
+        this.optimizationPanel.setSolution(solution);
+        this.optimizationPanel.repaint();
+    }
+
+    /**
+     * Function that returns the buttons.
+     * @return ArrayList<SquareButton> with the buttons.
+     */
+    public ArrayList<SquareButton> getButtons() {
+        return this.optimizationPanel.getSolutionArray();
+    }
+
+    /**
+     * Function that returns the buttons.
+     * @return ArrayList<SquareButton> with the buttons.
+     */
+    public SquareButton[][] getGrid() {
+        return this.optimizationPanel.getGridButtons();
+    }
+
+    /**
+     * Function that forwards the set of the image to the solution image.
+     */
+    public void setSolutionAsImage(){
+        this.referencePanel.setSolutionAsImage();
+        this.referencePanel.paintComponent(this.referencePanel.getGraphics());
+    }
+}
---
+++ src/main/java/org/soyaga/examples/Zip/MathModelZip/UI/Panels/StatsPanel.java
@@ -0,0 +1,79 @@
+package org.soyaga.examples.Zip.MathModelZip.UI.Panels;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * JPanel with the stats from the optimization.
+ */
+public class StatsPanel extends JPanel {
+    /**
+     * BufferedImage that with the default image.
+     */
+    private BufferedImage defaultImage;
+    /**
+     * JTextArea where to write the optimization info.
+     */
+    private final JTextArea textArea;
+    /**
+     * JScrollPane used to contain the JTextArea
+     */
+    private final JScrollPane scrollPane;
+
+    /**
+     * Constructor
+     */
+    public StatsPanel() {
+        super();
+        try {
+            this.defaultImage = ImageIO.read(Objects.requireNonNull(StatsPanel.class.getResource("/DefaultImages/Default_Math_Model_Image.png")));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        this.setPreferredSize(new Dimension(200,200));
+        this.textArea = new JTextArea();
+        this.scrollPane = new JScrollPane(this.textArea);
+    }
+
+    /**
+     * Function that initializes the JPanel components.
+     */
+    public void initComponent() {
+        this.textArea.setEditable(false);
+        this.scrollPane.setVisible(false);
+        this.setLayout(new BorderLayout());
+        this.add(this.scrollPane, BorderLayout.CENTER);
+    }
+
+    /**
+     * Function to set the solution Text.
+     * @param solutionText String with the solution text.
+     */
+    public void setSolutionText(String solutionText){
+        SwingUtilities.invokeLater(() -> {
+            this.textArea.append(solutionText);
+            this.textArea.setCaretPosition(this.textArea.getDocument().getLength());
+            this.scrollPane.setVisible(true);
+            this.revalidate();
+            this.repaint();
+        });
+    }
+
+    /**
+     * Function call in the graphical context to display the component.
+     * @param g  the <code>Graphics</code> context in which to paint
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (this.textArea.getText().trim().isEmpty()) {
+            g.drawImage(this.defaultImage.getScaledInstance(
+                            this.getWidth(), this.getHeight(), Image.SCALE_SMOOTH),
+                    0, 0, this);
+        }
+    }
+}
