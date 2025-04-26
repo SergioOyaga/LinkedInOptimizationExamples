9f571c4ac3cc845e4727bd1fbd82452040c0a748
Sergio Oyaga <44228600+SergioOyaga@users.noreply.github.com>
2025-04-26 20:57:23 +0200

    GeneticQueens Main Run

---
+++ src/main/java/org/soyaga/examples/Queens/GeneticQueens/RunLinkedInQueens.java
@@ -0,0 +1,11 @@
+package org.soyaga.examples.Queens.GeneticQueens;
+
+import java.io.IOException;
+
+public class RunLinkedInQueens {
+
+    public static void main(String[] args) throws IOException {
+        Controller controller = new Controller();
+        controller.startApp();
+    }
+}
