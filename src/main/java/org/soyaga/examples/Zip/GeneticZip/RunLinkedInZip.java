645a9ce98d9312e17a15d0273c4eebc15842042e
Sergio Oyaga <44228600+SergioOyaga@users.noreply.github.com>
2025-05-02 18:38:53 +0200

    GeneticZIP Run Main

---
+++ src/main/java/org/soyaga/examples/Zip/GeneticZip/RunLinkedInZip.java
@@ -0,0 +1,12 @@
+package org.soyaga.examples.Zip.GeneticZip;
+
+
+import java.io.IOException;
+
+public class RunLinkedInZip {
+
+    public static void main(String[] args) throws IOException {
+        Controller controller = new Controller();
+        controller.startApp();
+    }
+}
