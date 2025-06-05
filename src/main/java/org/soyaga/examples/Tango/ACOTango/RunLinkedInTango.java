6eb593bf9d652d5bd4f70a1c5c3f3c7f319bcf9e
Sergio Oyaga <44228600+SergioOyaga@users.noreply.github.com>
2025-06-05 16:55:12 +0200

    TangoAco, Run main

---
+++ src/main/java/org/soyaga/examples/Tango/ACOTango/RunLinkedInTango.java
@@ -0,0 +1,12 @@
+package org.soyaga.examples.Tango.ACOTango;
+
+
+import java.io.IOException;
+
+public class RunLinkedInTango {
+
+    public static void main(String[] args) throws IOException {
+        Controller controller = new Controller();
+        controller.startApp();
+    }
+}
